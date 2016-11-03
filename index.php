ae65abdadd15d3e4f67cb3c040f5459defc821a1
Justin D Crooke <Justin D Crooke>
2016-11-03 22:43:09 +0000

    yet another commit

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<?php ?>
-<head><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-	<script type="text/javascript" src="<?= base_url() ?>js/jquery-ui-1.12.0.custom/external/jquery/jquery.js"></script>
-	<script type="text/javascript" src="<?= base_url() ?>js/jquery-ui-1.12.0.custom/jquery-ui.min.js"></script>
-	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
-	<script type="text/javascript" src="<?= base_url() ?>js/fullcalendar-scheduler/lib/moment.min.js"></script>
-	<script type="text/javascript" src="<?= base_url() ?>js/fullcalendar-scheduler/lib/fullcalendar.min.js"></script>
-	<script type="text/javascript" src="<?= base_url() ?>js/fullcalendar-scheduler/scheduler.js"></script>
-	<script type="text/javascript" src="<?= base_url() ?>js/Chart.js/Chart.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
-
-	<title>PortersDesk.io DeskTop</title>
-
-  <body>
-
-
-    <div id="outer-logo-container" class="outer-logo-container-closed">
-        <div id="message">
-        </div>
-      </div>
-
-  </body>
+<?php
+$mysql = new mysqli("localhost", "root");
+echo "MySQL Server info: ".$mysql->host_info;
+?>
